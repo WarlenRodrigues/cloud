5e82434d4150df19e6b95ee4945acd289d05cf04
Warlen Rodrigues <warlen.cesarrodrigues@gmail.com>
2020-12-01 22:31:13 -0300

    [FEAT] Free firewal

--- ohio.py
+++ ohio.py
@@ -36,6 +36,7 @@ class PostgresProvider():
                             sudo -u postgres sh -c "psql -c \\"CREATE USER cloud WITH PASSWORD 'cloud';\\" && createdb -O cloud tasks"
                             sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf      
                             sudo sed -i "a\host all all 0.0.0.0/0 md5" /etc/postgresql/10/main/pg_hba.conf
+                            sudo ufw allow 5432/tcp
                             sudo systemctl restart postgresql
                             '''
 
