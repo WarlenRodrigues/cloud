85a3747d8f2de68de3c7fdc27ca7d79001efc812
Warlen Rodrigues <warlen.cesarrodrigues@gmail.com>
2020-12-01 22:14:10 -0300

    [FEAT] PostgreSQL server 95% ready... Need to create key

--- my_sql_user_set.sql
+++
@@ -1,3 +0,0 @@
-DROP USER IF EXISTS 'tasklist_app'@'localhost';
-CREATE USER 'tasklist_app'@'localhost' IDENTIFIED BY "DBServer-EC2";
-GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist.* TO 'tasklist_app'@'localhost';
--- ohio.py
+++ ohio.py
@@ -1,136 +1,159 @@
 from dotenv import load_dotenv
 import boto3
 import os
+import time
 
 # setting up aws credentials with .env lib
 load_dotenv()
 
-AWS_DB_REGION = os.getenv("AWS_DB_REGION")
-IMAGE_ID = 'ami-0dd9f0e7df0f0a138'
 
-DEFAULT_TAGS = [
-    {
-        'ResourceType': 'instance',
-        'Tags': [
+class PostgresProvider():
+    def __init__(self):
+        print("Setting variables... \n")
+        self.region = os.getenv("AWS_DB_REGION")
+        self.image_id = 'ami-0dd9f0e7df0f0a138'
+        self.ec2_tags = [
             {
-                'Key': 'Name',
-                'Value': 'postgreSQL Host Server'
-            },
-            {
-                'Key': 'Owner',
-                'Value': 'warlen'
-            },
-        ]
-    }
-]
-
-userdata = '''#!/bin/bash
-sudo apt update
-sudo apt install postgresql postgresql-contrib -y
-sudo -u postgres sh -c "psql -c \\"CREATE USER cloud WITH PASSWORD 'cloud';\\" && createdb -O cloud tasks"
-sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf      
-sudo sed -i "a\host all all 0.0.0.0/0 md5" /etc/postgresql/10/main/pg_hba.conf
-sudo systemctl restart postgresql
-'''
-
-# Boto3 AWS entrypoints
-db_aws_session = boto3.session.Session(region_name=AWS_DB_REGION)
-ec2 = db_aws_session.resource('ec2')
-client = db_aws_session.client('ec2', region_name=AWS_DB_REGION)
-
-# Cleaning AWS env
-# Get current running instances
-running_instances = []
-
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-for instance in instances:
-    running_instances.append(instance.id)
-
-# Terminate all instances before start creation proccess
-ec2.instances.filter(InstanceIds=running_instances).terminate()
-
-# Deleting Security Groups
-sg = client.describe_security_groups()
-for sec in sg["SecurityGroups"]:
-    if sec['GroupName'] == 'postgresDB':
-        client.delete_security_group(
-            GroupId=sec["GroupId"], GroupName=sec["GroupName"])
-
-# Preparing AWS env
-# Create Sec Group to give postgreSQL necessary iu and out permissions
-
-response = client.describe_vpcs()
-vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
-
-response = None
-
-try:
-    response = client.create_security_group(
-        Description='Allow all IPs to reach our postgres db',
-        GroupName='postgresDB',
-        VpcId=vpc_id,
-        TagSpecifications=[
-            {
-                'ResourceType': 'security-group',
+                'ResourceType': 'instance',
                 'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'postgreSQL Host Server'
+                    },
                     {
                         'Key': 'Owner',
                         'Value': 'warlen'
                     },
                 ]
-            },
-        ],
-        DryRun=True
-    )
-    print("Succeed @ creating Security Group postgresDB")
+            }
+        ]
+        self.created_sg_id = None
+        self.instances = None
 
-    security_group_id = response['GroupId']
+        self.userdata = '''#!/bin/bash
+                            sudo apt update
+                            sudo apt install postgresql postgresql-contrib -y
+                            sudo -u postgres sh -c "psql -c \\"CREATE USER cloud WITH PASSWORD 'cloud';\\" && createdb -O cloud tasks"
+                            sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/10/main/postgresql.conf      
+                            sudo sed -i "a\host all all 0.0.0.0/0 md5" /etc/postgresql/10/main/pg_hba.conf
+                            sudo systemctl restart postgresql
+                            '''
 
-    allowing_ingress = client.authorize_security_group_ingress(
-        GroupId=security_group_id,
-        IpPermissions=[
-            {'IpProtocol': 'tcp',
-                'FromPort': 5432,
-                'ToPort': 5432,
-                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-        ])
+        # Boto3 AWS entrypoints
+        print("Creating AWS connections... \n")
+        self.session = boto3.session.Session(region_name=self.region)
+        self.client = self.session.client('ec2', region_name=self.region)
+        self.ec2 = self.session.resource('ec2')
+        print("AWS connections created... \n")
 
-    allowing_egress = client.authorize_security_group_egress(
-        GroupId=security_group_id,
-        IpPermissions=[
-            {'IpProtocol': 'tcp',
-                'FromPort': 5432,
-                'ToPort': 5432,
-                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-        ])
+    def clean_aws_env(self):
+        print(f"Cleaning {self.region} region... \n")
+        running_instances = []
 
-    print("All permissions set @ postgresDB Security Group")
-except:
-    print("Failed @ creating Security Group postgresDB")
+        # Cleaning AWS env
+        instances = self.ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        for instance in instances:
+            running_instances.append(instance.id)
+
+        # Terminate all instances before start creation proccess
+        self.ec2.instances.filter(InstanceIds=running_instances).terminate()
+        print(f"Killing {self.region} instances... \n")
+        time.sleep(30)
+        print(f"{self.region} instances killed... \n")
+        # Deleting Security Groups
+        sg = self.client.describe_security_groups()
+        for sec in sg["SecurityGroups"]:
+            if sec['GroupName'] == 'postgresDB':
+                self.client.delete_security_group(
+                    GroupId=sec["GroupId"], GroupName=sec["GroupName"])
+        print(f"{self.region} security groups deleted... \n")
+
+    def create_sg(self, name):
+        print(f"Creating security group... \n")
+
+        res = self.client.describe_vpcs()
+        vpc_id = res.get('Vpcs', [{}])[0].get('VpcId', '')
+
+        try:
+            response = self.client.create_security_group(
+                Description='Allow all IPs to reach our postgres db',
+                GroupName='postgresDB',
+                VpcId=vpc_id,
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'security-group',
+                        'Tags': [
+                            {
+                                'Key': 'Owner',
+                                'Value': 'warlen'
+                            },
+                        ]
+                    },
+                ])
+
+            self.created_sg_id = response['GroupId']
+
+            try:
+                allowing_ingress = self.client.authorize_security_group_ingress(
+                    GroupId=self.created_sg_id,
+                    IpPermissions=[
+                        {'IpProtocol': 'tcp',
+                            'FromPort': 5432,
+                            'ToPort': 5432,
+                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+                    ])
+
+                allowing_egress = self.client.authorize_security_group_egress(
+                    GroupId=self.created_sg_id,
+                    IpPermissions=[
+                        {'IpProtocol': 'tcp',
+                            'FromPort': 5432,
+                            'ToPort': 5432,
+                            'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+                    ])
+
+                print("All permissions set @ postgresDB Security Group... \n")
+            except:
+                print("Failed setting permissions to postgresDB Security Group! \n")
+
+            print("Succeed @ creating Security Group postgresDB... \n")
+
+        except:
+            print("Failed @ creating Security Group postgresDB! \n")
+
+    def create_instace(self):
+        print("Creating EC2 instance postgreSQL Host Server... \n")
+        self.instances = self.ec2.create_instances(
+            InstanceType="t2.micro",
+            ImageId=self.image_id,
+            MinCount=1,
+            MaxCount=1,
+            KeyName="db_servers",
+            TagSpecifications=self.ec2_tags,
+            SecurityGroups=['postgresDB'],
+            UserData=self.userdata
+        )
+        print("Created EC2 instance postgreSQL Host Server... \n")
+
+    def setting_env_up(self):
+        # Create Sec Group to give postgreSQL necessary iu and out permissions
+        self.create_sg('postgresDB')
+        # Create Instance to run PostgreSQL and provide DB
+        self.create_instace()
+        # must run $sudo chmod 600 /path/to/my/key.pem to ensure key security if wants to ssh the instance
+
+    def get_instance_ip(self):
+        print("Getting server public IP Address... This may take a while... \n")
+        time.sleep(30)
+        response = self.client.describe_instances(
+            InstanceIds=[self.instances[0].id])
+        instance = response['Reservations'][0]['Instances'][0]
+        ip = instance['PublicIpAddress']
+        print(f"PostgreSQL running at {ip}")
 
 
-# Create Instance to run MySQL and provide DB
-ec2.create_instances(
-    InstanceType="t2.micro",
-    ImageId=IMAGE_ID,
-    MinCount=1,
-    MaxCount=1,
-    KeyName="db_servers",
-    TagSpecifications=DEFAULT_TAGS,
-    SecurityGroups=["postgresDB"],
-    UserData=userdata
-)
-# must run $sudo chmod 600 /path/to/my/key.pem to ensure key security if wants to ssh the instance
-
-running_instances = []
-
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-for instance in instances:
-    print(instance.id, instance.instance_type)
-
-res = client.describe_instances(InstanceIds=[running_instances[0], ])
-ip = res['Reservations'][0]['Instances'][0]["PublicIpAddress"]
-
-print(ip)
+server = PostgresProvider()
+server.clean_aws_env()
+server.setting_env_up()
+server.get_instance_ip()
